3f3b7028b6954aa8b0dadba28510d1126acb5d6f
Jovi0li <jovi.oli04@gmail.com>
2024-01-03 22:54:15 -0300

    feat: add user actions

---
+++ internal/user/actions.go
@@ -0,0 +1,157 @@
+package user
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/JoVi0li/ocean-server/internal/database"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+var service Service
+
+func Configure() {
+	service = Service{
+		Repository: &RepositoryPostgres{
+			Connection: database.Conn,
+		},
+	}
+}
+
+func CreateUsers(ctx *gin.Context) {
+	var user User
+
+	if err := ctx.BindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	sucess, err := service.Create(ctxTimeout, user)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"sucess": true,
+		"data":   sucess,
+		"error":  nil,
+	})
+}
+
+func GetUsers(ctx *gin.Context) {
+	param := ctx.Param("id")
+
+	if param == "" {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  ErrorIdInvalid,
+		})
+
+		return
+	}
+
+	parsedId, err := uuid.Parse(param)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  ErrorIdInvalid,
+		})
+
+		return
+	}
+
+	user, err := service.FindById(ctx, parsedId)
+
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+
+		if err == ErrorUserNotFound {
+			statusCode = http.StatusNotFound
+		}
+
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  statusCode,
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusNotFound, gin.H{
+		"sucess": true,
+		"data":   user,
+		"error":  nil,
+	})
+}
+
+func DeleteUsers(ctx *gin.Context) {
+	param := ctx.Param("id")
+
+	if param == "" {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  ErrorIdInvalid,
+		})
+
+		return
+	}
+
+	parsedId, err := uuid.Parse(param)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  ErrorIdInvalid,
+		})
+
+		return
+	}
+
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	if err := service.DeleteById(ctxTimeout, parsedId); err != nil {
+		statusCode := http.StatusInternalServerError
+
+		if err == ErrorUserNotFound {
+			statusCode = http.StatusNotFound
+		}
+
+		ctx.JSON(statusCode, gin.H{
+			"sucess": false,
+			"data":   nil,
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, gin.H{
+		"sucess": true,
+		"data":   nil,
+		"error":  nil,
+	})
+}
