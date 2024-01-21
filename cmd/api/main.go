a09bfa87635c60b2f33a594b0e7111664a192cd0
jovioli <jovioli.dev04@gmail.com>
2024-01-21 16:23:03 -0300

    feat: setup environment variables and database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
 	"github.com/JoVi0li/ocean-server/internal/auth"
+	"github.com/JoVi0li/ocean-server/internal/database"
 	"github.com/JoVi0li/ocean-server/internal/user"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	/// Setup server
 	server := gin.Default()
+
+	/// Setup environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	/// Setup database
+	_, err := database.NewConnection(context.Background(), os.Getenv("POSTGRES_CONN_STR"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	/// Setup user
 	user.Configure()
