171d6786f436f5681614fa962ee6daca6106db98
jovioli <jovioli.dev04@gmail.com>
2024-01-31 13:32:14 -0300

    feat: add streaming service

--- internal/modules/streaming/repository.go
+++ internal/modules/streaming/repository.go
@@ -8,15 +8,17 @@ import (
 )
 
 type Repository interface {
-	CreateCall(ctx context.Context, call Call, users []UsersInCall) error
-	UpdateCall(ctx context.Context, call Call) error
+	CreateCall(ctx context.Context, users [2]UsersInCall) (Call, error)
+	UpdateCall(ctx context.Context, id string) error
 }
 
 type RepositoryPostgres struct {
 	Connection *pgxpool.Pool
 }
 
-func (r *RepositoryPostgres) CreateCall(ctx context.Context, call Call, users [2]UsersInCall) (Call, error) {
+func (r *RepositoryPostgres) CreateCall(ctx context.Context, users [2]UsersInCall) (Call, error) {
+	var call Call
+
 	err := r.Connection.QueryRow(
 		ctx,
 		"BEGIN;INSERT INTO voice_calls RETURNING id INTO result_id, started_at, finished_at;ROLLBACK;INSERT INTO users_in_calls (user_id, voice_call_id) VALUES ($1, result_id);ROLLBACK;INSERT INTO users_in_calls (user_id, voice_call_id) VALUES ($2, result_id);ROLLBACK;COMMIT;",
@@ -24,17 +26,17 @@ func (r *RepositoryPostgres) CreateCall(ctx context.Context, call Call, users [2
 		users[1].ID,
 	).Scan(&call.ID, &call.StartedAt, &call.FinishedAt)
 	if err != nil {
-		return Call{}, err
+		return call, err
 	}
 
 	return call, nil
 }
 
-func (r *RepositoryPostgres) UpdateCall(ctx context.Context, call Call) error {
+func (r *RepositoryPostgres) UpdateCall(ctx context.Context, id string) error {
 	tag, err := r.Connection.Exec(
 		ctx,
 		"UPDATE voice_calls SET finished_at = CURRENT_TIMESTAMP WHERE id = $1 AND finished_at IS NULL;",
-		call.ID,
+		id,
 	)
 	if tag.RowsAffected() == 0 {
 		return shared.ErrorUserNotFound
